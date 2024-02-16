e54ffbac4b7b56ce79047126e2a0a09c33b3a20e
rohit dhore <78274171+rohit9545@users.noreply.github.com>
2024-02-16 14:56:10 +0530

    Create CompanyController.java

---
+++ CompanyController.java
@@ -0,0 +1,30 @@
+CompanyController.java
+@RestController
+@RequestMapping(&quot;/companies&quot;)
+public class CompanyController {
+
+@Autowired
+private CompanyService companyService;
+
+@GetMapping(&quot;/&quot;)
+public List&lt;Company&gt; searchCompaniesByEmployeeRange(@RequestParam int
+minEmployees, @RequestParam int maxEmployees) {
+return companyService.findCompaniesByEmployeeRange(minEmployees, maxEmployees);
+}
+
+@GetMapping(&quot;/{userId}/clients&quot;)
+public List&lt;Client&gt; getClientsByUser(@PathVariable Long userId) {
+return companyService.findClientsByUser(userId);
+}
+
+@GetMapping(&quot;/search&quot;)
+public List&lt;Company&gt; searchCompaniesByName(@RequestParam String name) {
+return companyService.findCompaniesByName(name);
+
+}
+
+@GetMapping(&quot;/max-revenue&quot;)
+public List&lt;Company&gt; getCompaniesWithMaxRevenue() {
+return companyService.findCompaniesWithMaxRevenue();
+}
+}
