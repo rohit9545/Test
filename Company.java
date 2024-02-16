e5ae225292698a5d6d7a6c732adc80633719ac80
rohit dhore <78274171+rohit9545@users.noreply.github.com>
2024-02-16 14:52:50 +0530

    Create Company.java

---
+++ Company.java
@@ -0,0 +1,51 @@
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Company {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+private String name;
+private int employees;
+private double revenue;
+
+// Getters and setters
+public Long getId() {
+return id;
+}
+
+public void setId(Long id) {
+this.id = id;
+}
+
+public String getName() {
+return name;
+}
+
+public void setName(String name) {
+this.name = name;
+}
+
+public int getEmployees() {
+return employees;
+}
+
+public void setEmployees(int employees) {
+this.employees = employees;
+}
+
+public double getRevenue() {
+return revenue;
+}
+
+public void setRevenue(double revenue) {
+this.revenue = revenue;
+}
+
+}
